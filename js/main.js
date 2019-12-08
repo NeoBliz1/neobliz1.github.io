c3f13a92da43b5c1364a2ebc56650c426fcfa3fe
Ilya <49137013+NeoBliz1@users.noreply.github.com>
2019-12-08 12:55:34 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function zoomInHeader () {
+	//zoom in main header
+	$(".headerContent").addClass('zoomIn animated');
+	//hide elements which will be animated
+	$('.scrollAnimate').addClass('hidden');
+}
+function scrollAnimate () {
+	//use waypoint JQuery plugin for tracking scroll elements
+	$('.scrollAnimate').waypoint(function(direction) {		
+		if (direction === 'up') {			
+			//animated fadeOut elements when their are scrolling 
+			$(this.element)			
+			.removeClass('fadeInUp animated slow')
+			.addClass('fadeOutDown animated');
+		}
+		else {			
+			//animated fadIn elements when their are scrolling 
+			$(this.element)
+			.removeClass('hidden fadeOutDown animated')
+			.addClass('fadeInUp animated slow');			
+		}		
+	}, 
+	{	
+		//offset from window top
+	  offset: '85%'
+	});
+}
+function loaderScreen() {
+	$('.loader-gif').fadeOut('slow');
+}
+
+/*main block */
+$( window ).on( "load", function() {
+  console.log( "document loaded" );
+  loaderScreen();
+  zoomInHeader();
+	scrollAnimate ();
+});
