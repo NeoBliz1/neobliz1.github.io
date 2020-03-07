8847a0c74db6cc80dcbb87b2e23747363d710eb0
Ilya <49137013+NeoBliz1@users.noreply.github.com>
2020-03-07 14:42:01 +0500

    Add files via upload

--- js/projectPage.js
+++ js/projectPage.js
@@ -5,17 +5,9 @@ var loaderScreen = function () {
 	$('.loader-gif').fadeOut('slow');
 }
 
-var createCodeSnippet = function () {
-	var scriptText;
-	$.get('https://res.cloudinary.com/deah4rwon/raw/upload/v1581477877/js/code_project3_xurmh5.txt', function (response) {
-    scriptText = response;
 
-    $('.snippetWrapper').html(scriptText);
-	});	
-}
 /**************main block*******************/
 $( window ).on( 'load', function() {
   console.log( 'document loaded' );
-  loaderScreen();
-  createCodeSnippet();	
+  loaderScreen();  	
 });
--- js/windowResizeHandler.js
+++ js/windowResizeHandler.js
@@ -1,55 +1,70 @@
 'use strict';
-
-var windowSizeHandler = function () {
-	if ( $('.dynamicallyResize').length ) {			
-		$('.dynamicallyResize').scaleDivMax();
+var viewportWidth;
+var biggerThanFT = false;
+var windowSizeHandler = function () {			
+	viewportWidth = $(window).width();
+	/*console.log(viewportWidth);*/
+	if ($('.dynamicallyResize').length && viewportWidth < 401) {		
+		$('.dynamicallyResize').scaleDivMax();	
+		biggerThanFT = true;	
 	}
+	else {
+		while (biggerThanFT){
+			$('.dynamicallyResize').scaleDivMax();	
+			biggerThanFT = false;			
+		}	
+	}	
 }	
 
 var DSdurationTime = 1000;//div skill full size animation duration time
-	
+var divTargetCoeff=1;	
 //jQuery function for scaling div
-jQuery.fn.scaleDivMax = function () {
-	var viewportHeight = $(window).height();
-	var viewportWidth = $(window).width();
+jQuery.fn.scaleDivMax = function () {	
+	var viewportHeight = $(window).height();	
 	var divCurrentHeight = $(this).outerHeight();
 	var divCurrentWidth = $(this).outerWidth();	
-	console.log(divCurrentWidth+' '+divCurrentHeight);
-	var widhtScalePercent = $(this).attr('data');
-	var divTargetWidthCoeff = viewportWidth*widhtScalePercent/divCurrentWidth;
-	var divTargetHeightCoeff = viewportHeight*widhtScalePercent/divCurrentHeight;
-	var divTargetCoeff;
-	if (viewportWidth>viewportHeight && (viewportWidth-viewportHeight)>200) {
-		divTargetCoeff = divTargetHeightCoeff;
+	/*console.log(divCurrentWidth+' '+divCurrentHeight);*/
+	if (viewportWidth < 401) {
+		var widhtScalePercent = $(this).attr('data');
+		var divTargetWidthCoeff = viewportWidth*widhtScalePercent/divCurrentWidth;
+		var divTargetHeightCoeff = viewportHeight*widhtScalePercent/divCurrentHeight;
+		
+		if (viewportWidth>viewportHeight && (viewportWidth-viewportHeight)>200) {
+			divTargetCoeff = divTargetHeightCoeff;
+		}
+		else {
+			divTargetCoeff = divTargetWidthCoeff;			
+		}		 
 	}
-	else {
-		divTargetCoeff = divTargetWidthCoeff;
-	}
+	else {		
+		divTargetCoeff = 1;		 
+	}		
 	
 	this.animate(
 		{  
 			textIndent: divTargetCoeff
 		}, 
 		{
-				step: function(now,fx) 
-			{
-				$(this).css('-webkit-transform','scale3d('+now+', '+now+', '+now+')');
-				$(this).css('transform','scale3d('+now+', '+now+', '+now+')');  
-			}, 
-			duration: DSdurationTime,
-			queue: false,
-			complete: function(){					
+			step: function(now,fx) 
+		{
+			$(this).css('-webkit-transform','scale3d('+now+', '+now+', '+now+')');
+			$(this).css('transform','scale3d('+now+', '+now+', '+now+')');  
+		}, 
+		duration: DSdurationTime,
+		queue: false,
+		complete: function(){					
 			}
 		},
-			'linear'
-	)
+		'linear'
+	)	
+		
 	
 	return this;
 }
 	
 var resizeTimer;
 windowSizeHandler();//running once
-$(window).resize(function() {
+$(window).resize(function() {	
 	clearTimeout(resizeTimer);
 	resizeTimer = setTimeout(function() {
 		windowSizeHandler();
