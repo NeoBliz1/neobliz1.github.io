d0ecde118c1812732f3e33aeda7de234278eecb7
Ilya <49137013+NeoBliz1@users.noreply.github.com>
2020-10-11 20:07:58 +0500

    Update project5.js

--- js/projects/project5.js
+++ js/projects/project5.js
@@ -679,7 +679,16 @@ let gameSketch = function(p) {
   };
   $('#canvasHolderSmall').contextmenu(function() {
     return false;
-  }).css('touch-action', 'manipulation');
+  }).css({
+    'touch-action': 'manipulation',
+    '-webkit-touch-callout': 'none', /* iOS Safari */
+    '-webkit-user-select': 'none', /* Safari */
+    ' -khtml-user-select': 'none', /* Konqueror HTML */
+       '-moz-user-select': 'none', /* Old versions of Firefox */
+        '-ms-user-select': 'none', /* Internet Explorer/Edge */
+            'user-select': 'none' /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+  });;
   
 };
 
