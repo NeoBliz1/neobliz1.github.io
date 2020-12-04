5c92282e5936fe26cd97765350953b31aad49c33
Ilya <49137013+NeoBliz1@users.noreply.github.com>
2020-12-04 07:23:27 +0500

    tune scrollMagick

--- js/main.js
+++ js/main.js
@@ -6,10 +6,9 @@ jQuery.event.special.renewPage = {
 };
 
 //checking browser and platform
-var platformIsMobile = false;
+var platformIsMobile = (function(a){return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4));})(navigator.userAgent||navigator.vendor||window.opera);
 var orientationIsChange = false;
-if (navigator.userAgent.match(/(iPod|iPhone|iPad)/) || navigator.userAgent.match(/(Android)/)) {
-	platformIsMobile = true;
+if (platformIsMobile) {
 	$(window).on( 'orientationchange', function( event ) {
 		$('.loader-gif').css('display', 'initial');
 		orientationIsChange = true;
@@ -20,95 +19,128 @@ if (navigator.userAgent.match(/(iPod|iPhone|iPad)/) || navigator.userAgent.match
 var zoomInHeader = function () {
 	//zoom in main header
 	$('.headerContent').addClass('zoomIn animated');
-	//hide elements which will be animated
-	$('.scrollAnimate').addClass('hidden');
+	//hide are elements which will be animated
+	$('.scrollAnimate').addClass('hidden');	
 }
 this.doAnim = false;
 /*use waypoint JQuery plugin for tracking scroll elements*/
 var scrollAnimate = function (offsetNum, upButOffsetNum) {	
-	$(window).scroll(function(event) {
-		console.log($(window).scrollTop())
-	});
-	$('.scrollAnimate')
-	.waypoint(
-		function(direction) {
-			if (direction === 'up') {			
-				//animated fadeOut elements when their are scrolling 
-				$(this.element)			
-				.removeClass('fadeInUp animated slow')
-				.addClass('fadeOutDown animated');
-			}
-			else {			
-				//animated fadIn elements when their are scrolling 
-				$(this.element)
-				.removeClass('hidden fadeOutDown animated')
-				.addClass('fadeInUp animated slow');			
-			}					
-		}, 
-		{	offset: offsetNum}
-	);
+	var scrollEventOffset = -300;
 
-	$('.upButton')
-	.waypoint(
-		function(direction) {
-			if (direction === 'up') {			
-				//animated fadeOut elements when their are scrolling 
-				$(this.element)			
-				.removeClass('fadeInUp animated slow')
-				.addClass('fadeOutDown animated');
-			}
-			else {			
-				//animated fadIn elements when their are scrolling 
-				$(this.element)
-				.removeClass('hidden fadeOutDown animated')
-				.addClass('fadeInUp animated slow');			
-			}					
-		}, 
-		{	offset: upButOffsetNum}
-	);
-	// if (!platformIsMobile) {
-	// if (true) {
-		var $fluidMeterScroll = $('.fluidMeterScroll')
-		//activated canvas when it is in the visible state
-		$fluidMeterScroll
-		.waypoint(
-			function(direction) {		
-				if (direction === 'up') {			
-					doAnim = false;//global anim variable
-					// console.log('anim stop')
-				}
-				else {
-					doAnim = true;//global anim variable
-					htmlFM.restart();
-					cssFM.restart();
-					jsFM.restart();
-					jQueryFM.restart();
-					pythonFM.restart();	
-					// console.log('anim start1')				
-				}			
-			}, 
-			{	offset: '100%'}
-		);
-		$fluidMeterScroll.waypoint(
-			function(direction) {		
-				if (direction === 'down') {			
-					doAnim = false;//global anim variable
-					// console.log('anim stop')
-				}
-				else {
-					doAnim = true;//global anim variable
-					htmlFM.restart();
-					cssFM.restart();
-					jsFM.restart();
-					jQueryFM.restart();
-					pythonFM.restart();
-					// console.log('anim start2')						
-				}	
-			// console.log(direction);
-			}, 
-			{	offset: '-15%'}
-		);
-	// }		
+	// init controller
+	var controller1 = new ScrollMagic.Controller();
+	
+	$('.scrollAnimate').addClass('animated')
+	.each(function(index, el) {
+		var stickyId = 'myStickyEl'+index;
+		var sceneNameindex;
+		console.log(sceneName+index)
+		
+		$(this).attr('id', stickyId);
+		// create a scene	
+		sceneName = new ScrollMagic.Scene({
+			triggerElement: stickyId,												
+			offset: scrollEventOffset // move trigger to value
+		})	
+		.addTo(controller1);
+
+		sceneName.on("start", function (event) {
+		 	if (event.scrollDirection === 'FORWARD') {	 		
+		 		console.log('scrollDirection FORWARD');	 		
+		 		$(stickyId)
+				.removeClass('hidden fadeOutDown')
+				.addClass('fadeInUp slow');
+		 	}
+		 	else {
+		 		$(stickyId)
+				.removeClass('fadeInUp slow')
+				.addClass('fadeOutDown');
+		 	}
+		});	
+	});
+
+	
+
+	// $('.scrollAnimate')
+	// .waypoint(
+	// 	function(direction) {
+	// 		if (direction === 'up') {			
+	// 			//animated fadeOut elements when their are scrolling 
+	// 			$(this.element)			
+	// 			.removeClass('fadeInUp animated slow')
+	// 			.addClass('fadeOutDown animated');
+	// 		}
+	// 		else {			
+	// 			//animated fadIn elements when their are scrolling 
+	// 			$(this.element)
+	// 			.removeClass('hidden fadeOutDown animated')
+	// 			.addClass('fadeInUp animated slow');			
+	// 		}					
+	// 	}, 
+	// 	{	offset: offsetNum}
+	// );
+
+	// $('.upButton')
+	// .waypoint(
+	// 	function(direction) {
+	// 		if (direction === 'up') {			
+	// 			//animated fadeOut elements when their are scrolling 
+	// 			$(this.element)			
+	// 			.removeClass('fadeInUp animated slow')
+	// 			.addClass('fadeOutDown animated');
+	// 		}
+	// 		else {			
+	// 			//animated fadIn elements when their are scrolling 
+	// 			$(this.element)
+	// 			.removeClass('hidden fadeOutDown animated')
+	// 			.addClass('fadeInUp animated slow');			
+	// 		}					
+	// 	}, 
+	// 	{	offset: upButOffsetNum}
+	// );
+	// // if (!platformIsMobile) {
+	// // if (true) {
+	// 	var $fluidMeterScroll = $('.fluidMeterScroll')
+	// 	//activated canvas when it is in the visible state
+	// 	$fluidMeterScroll
+	// 	.waypoint(
+	// 		function(direction) {		
+	// 			if (direction === 'up') {			
+	// 				doAnim = false;//global anim variable
+	// 				// console.log('anim stop')
+	// 			}
+	// 			else {
+	// 				doAnim = true;//global anim variable
+	// 				htmlFM.restart();
+	// 				cssFM.restart();
+	// 				jsFM.restart();
+	// 				jQueryFM.restart();
+	// 				pythonFM.restart();	
+	// 				// console.log('anim start1')				
+	// 			}			
+	// 		}, 
+	// 		{	offset: '100%'}
+	// 	);
+	// 	$fluidMeterScroll.waypoint(
+	// 		function(direction) {		
+	// 			if (direction === 'down') {			
+	// 				doAnim = false;//global anim variable
+	// 				// console.log('anim stop')
+	// 			}
+	// 			else {
+	// 				doAnim = true;//global anim variable
+	// 				htmlFM.restart();
+	// 				cssFM.restart();
+	// 				jsFM.restart();
+	// 				jQueryFM.restart();
+	// 				pythonFM.restart();
+	// 				// console.log('anim start2')						
+	// 			}	
+	// 		// console.log(direction);
+	// 		}, 
+	// 		{	offset: '-15%'}
+	// 	);
+	// // }		
 }
 
 /*fadingOut loader screen*/
@@ -214,20 +246,20 @@ var windowSizeHandler = function (viewportWidth, viewportHeight) {
 	// Waypoint.destroyAll();
 	 //remove all waypoints	
 	
-	var upButOffsetNum=0, offsetNum=0;
-	//offset from window top
-	if (viewportHeight < 480) {
-		offsetNum = '95%';
-		upButOffsetNum = '60%';
-	}
-	else if (viewportHeight > 2000) {
-		offsetNum = '100%';
-		upButOffsetNum = '50%';	
-	}
-	else {
-		offsetNum = '90%';
-		upButOffsetNum = '40%';
-	};
+	// var upButOffsetNum=0, offsetNum=0;
+	// //offset from window top
+	// if (viewportHeight < 480) {
+	// 	offsetNum = '95%';
+	// 	upButOffsetNum = '60%';
+	// }
+	// else if (viewportHeight > 2000) {
+	// 	offsetNum = '100%';
+	// 	upButOffsetNum = '50%';	
+	// }
+	// else {
+	// 	offsetNum = '90%';
+	// 	upButOffsetNum = '40%';
+	// };
 		
 	// scrollAnimate(offsetNum, upButOffsetNum);
 
@@ -799,13 +831,8 @@ var resizeHandler = function () {
 			}			
 			// console.log('viewportHeight' + viewportHeight);
 			// console.log('windowOuterHeight'+windowOuterHeight, 'currBrowserHeight'+currBrowserHeight);
-			// console.log(bWratio, bHratio);
-			if (platformIsMobile && orientationIsChange) {
-				projectDivSizeHandler(viewportWidth, viewportHeight, wDPR, windowOuterHeight, browserInitialWidth);
-				cloneDivSizeHandler(viewportHeight, viewportWidth);
-				windowSizeHandler(viewportWidth, viewportHeight);
-			}
-			if(bWratio !== 1 || bHratio !== 1){
+			// console.log(bWratio, bHratio);			
+			if(platformIsMobile && orientationIsChange || bWratio !== 1 || bHratio !== 1){
 				browserInitialWidth = currBrowserWidth;
 				windowOuterHeight = currBrowserHeight;
 				projectDivSizeHandler(viewportWidth, viewportHeight, wDPR, windowOuterHeight, browserInitialWidth);
@@ -1046,6 +1073,7 @@ $(window).on( 'load', function() {
 	var wDPR = window.devicePixelRatio;
 	var windowOuterWidth = window.outerWidth;
 	var windowOuterHeight = window.outerHeight;
+	scrollAnimate();//scroll handler for divs
 	scrollTop();//handler for up button			
 	mySkillsAnimation();
 	projectDivSizeHandler(viewportWidth, viewportHeight, wDPR, windowOuterHeight, windowOuterWidth); //resize MySkills divs from window size	
