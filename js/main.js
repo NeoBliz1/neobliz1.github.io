3f71b932549d9fe395fb86f00cf6fcc670ea5c86
Ilya <49137013+NeoBliz1@users.noreply.github.com>
2022-08-20 04:41:23 -0400

    fix some recommends by eslint

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-"use strict";
+/* eslint-disable no-undef, @typescript-eslint/no-unused-vars, no-unused-vars */
+
 //srt named event for refresh parallax imgs
 jQuery.event.special.renewPage = {
   bindType: "resize",
@@ -11,7 +12,7 @@ var platformIsMobile = (function (a) {
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
       a
     ) ||
-    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(
       a.substr(0, 4)
     )
   );
@@ -357,13 +358,14 @@ var mySkillsAnimation = function () {
   ) {
     var divTargetWidthCoeff = (viewportWidth * 0.9) / divCurrentWidth;
     var divTargetHeightCoeff = (viewportHeight * 0.9) / divCurrentHeight;
+    var divTargetCoeff;
     if (
       viewportWidth > viewportHeight &&
       viewportWidth - viewportHeight > 200
     ) {
-      var divTargetCoeff = divTargetHeightCoeff;
+      divTargetCoeff = divTargetHeightCoeff;
     } else {
-      var divTargetCoeff = divTargetWidthCoeff;
+      ivTargetCoeff = divTargetWidthCoeff;
     }
     // console.log(viewportWidth);
     // console.log(viewportHeight);
@@ -1134,7 +1136,7 @@ var tMessageDialogBox = function (viewportWidth, viewportHeight) {
     });
     $("#tMessageArea").keypress(function (event) {
       var key = event.keyCode;
-      if (key == 13) {
+      if (key === 13) {
         $(".sendBtn").click();
       }
     });
@@ -1173,163 +1175,163 @@ var tMessageDialogBox = function (viewportWidth, viewportHeight) {
   $(".userIcon").fixUserIconSize();
 };
 //initialize my skills fluid meter
-{
-  //initialization HTML fluid meter
-  var FMfont = "Big Shoulders Display";
-  var htmlFM = new FluidMeter();
-  htmlFM.init({
-    targetContainer: document.getElementById("HTML-fluid-meter"),
-    fillPercentage: 75,
-    options: {
-      fontFamily: FMfont,
-      drawPercentageSign: true,
-      drawBubbles: true,
-      drawShadow: false,
-      size: 300,
-      borderWidth: 15,
-      backgroundColor: "#e2e2e2",
-      foregroundColor: "#fafafa",
-      foregroundFluidLayer: {
-        fillStyle: "red",
-        angularSpeed: 100,
-        maxAmplitude: 12,
-        frequency: 30,
-        horizontalSpeed: -75
-      },
-      backgroundFluidLayer: {
-        fillStyle: "pink",
-        angularSpeed: 100,
-        maxAmplitude: 15,
-        frequency: 30,
-        horizontalSpeed: 75
-      }
-    }
-  });
 
-  //initialization CSS fluid meter
-  var cssFM = new FluidMeter();
-  cssFM.init({
-    targetContainer: document.getElementById("CSS-fluid-meter"),
-    fillPercentage: 75,
-    options: {
-      fontFamily: FMfont,
-      drawPercentageSign: true,
-      drawBubbles: true,
-      drawShadow: false,
-      size: 300,
-      borderWidth: 15,
-      backgroundColor: "#e2e2e2",
-      foregroundColor: "#fafafa",
-      foregroundFluidLayer: {
-        fillStyle: "#563d7c",
-        angularSpeed: 100,
-        maxAmplitude: 12,
-        frequency: 30,
-        horizontalSpeed: -75
-      },
-      backgroundFluidLayer: {
-        fillStyle: "#ae7bfb",
-        angularSpeed: 100,
-        maxAmplitude: 15,
-        frequency: 30,
-        horizontalSpeed: 75
-      }
+//initialization HTML fluid meter
+var FMfont = "Big Shoulders Display";
+var htmlFM = new FluidMeter();
+htmlFM.init({
+  targetContainer: document.getElementById("HTML-fluid-meter"),
+  fillPercentage: 75,
+  options: {
+    fontFamily: FMfont,
+    drawPercentageSign: true,
+    drawBubbles: true,
+    drawShadow: false,
+    size: 300,
+    borderWidth: 15,
+    backgroundColor: "#e2e2e2",
+    foregroundColor: "#fafafa",
+    foregroundFluidLayer: {
+      fillStyle: "red",
+      angularSpeed: 100,
+      maxAmplitude: 12,
+      frequency: 30,
+      horizontalSpeed: -75
+    },
+    backgroundFluidLayer: {
+      fillStyle: "pink",
+      angularSpeed: 100,
+      maxAmplitude: 15,
+      frequency: 30,
+      horizontalSpeed: 75
     }
-  });
+  }
+});
 
-  //initialization JS fluid meter
-  var jsFM = new FluidMeter();
-  jsFM.init({
-    targetContainer: document.getElementById("JS-fluid-meter"),
-    fillPercentage: 75,
-    options: {
-      fontFamily: FMfont,
-      drawPercentageSign: true,
-      drawBubbles: true,
-      drawShadow: false,
-      size: 300,
-      borderWidth: 15,
-      backgroundColor: "#e2e2e2",
-      foregroundColor: "#fafafa",
-      foregroundFluidLayer: {
-        fillStyle: "#f1e05a",
-        angularSpeed: 100,
-        maxAmplitude: 12,
-        frequency: 30,
-        horizontalSpeed: -75
-      },
-      backgroundFluidLayer: {
-        fillStyle: "#f2e68d",
-        angularSpeed: 100,
-        maxAmplitude: 15,
-        frequency: 30,
-        horizontalSpeed: 75
-      }
+//initialization CSS fluid meter
+var cssFM = new FluidMeter();
+cssFM.init({
+  targetContainer: document.getElementById("CSS-fluid-meter"),
+  fillPercentage: 75,
+  options: {
+    fontFamily: FMfont,
+    drawPercentageSign: true,
+    drawBubbles: true,
+    drawShadow: false,
+    size: 300,
+    borderWidth: 15,
+    backgroundColor: "#e2e2e2",
+    foregroundColor: "#fafafa",
+    foregroundFluidLayer: {
+      fillStyle: "#563d7c",
+      angularSpeed: 100,
+      maxAmplitude: 12,
+      frequency: 30,
+      horizontalSpeed: -75
+    },
+    backgroundFluidLayer: {
+      fillStyle: "#ae7bfb",
+      angularSpeed: 100,
+      maxAmplitude: 15,
+      frequency: 30,
+      horizontalSpeed: 75
     }
-  });
+  }
+});
 
-  //initialization jQuery fluid meter
-  var jQueryFM = new FluidMeter();
-  jQueryFM.init({
-    targetContainer: document.getElementById("jQuery-fluid-meter"),
-    fillPercentage: 75,
-    options: {
-      fontFamily: FMfont,
-      drawPercentageSign: true,
-      drawBubbles: true,
-      drawShadow: false,
-      size: 300,
-      borderWidth: 15,
-      backgroundColor: "#e2e2e2",
-      foregroundColor: "#fafafa",
-      foregroundFluidLayer: {
-        fillStyle: "#ff9000",
-        angularSpeed: 100,
-        maxAmplitude: 12,
-        frequency: 30,
-        horizontalSpeed: -75
-      },
-      backgroundFluidLayer: {
-        fillStyle: "#ffc880",
-        angularSpeed: 100,
-        maxAmplitude: 15,
-        frequency: 30,
-        horizontalSpeed: 75
-      }
+//initialization JS fluid meter
+var jsFM = new FluidMeter();
+jsFM.init({
+  targetContainer: document.getElementById("JS-fluid-meter"),
+  fillPercentage: 75,
+  options: {
+    fontFamily: FMfont,
+    drawPercentageSign: true,
+    drawBubbles: true,
+    drawShadow: false,
+    size: 300,
+    borderWidth: 15,
+    backgroundColor: "#e2e2e2",
+    foregroundColor: "#fafafa",
+    foregroundFluidLayer: {
+      fillStyle: "#f1e05a",
+      angularSpeed: 100,
+      maxAmplitude: 12,
+      frequency: 30,
+      horizontalSpeed: -75
+    },
+    backgroundFluidLayer: {
+      fillStyle: "#f2e68d",
+      angularSpeed: 100,
+      maxAmplitude: 15,
+      frequency: 30,
+      horizontalSpeed: 75
     }
-  });
+  }
+});
 
-  //initialization python fluid meter
-  var pythonFM = new FluidMeter();
-  pythonFM.init({
-    targetContainer: document.getElementById("python-fluid-meter"),
-    fillPercentage: 75,
-    options: {
-      fontFamily: FMfont,
-      drawPercentageSign: true,
-      drawBubbles: true,
-      drawShadow: false,
-      size: 300,
-      borderWidth: 15,
-      backgroundColor: "#e2e2e2",
-      foregroundColor: "#fafafa",
-      foregroundFluidLayer: {
-        fillStyle: "#3572a5",
-        angularSpeed: 100,
-        maxAmplitude: 12,
-        frequency: 30,
-        horizontalSpeed: -75
-      },
-      backgroundFluidLayer: {
-        fillStyle: "#70b5ef",
-        angularSpeed: 100,
-        maxAmplitude: 15,
-        frequency: 30,
-        horizontalSpeed: 75
-      }
+//initialization jQuery fluid meter
+var jQueryFM = new FluidMeter();
+jQueryFM.init({
+  targetContainer: document.getElementById("jQuery-fluid-meter"),
+  fillPercentage: 75,
+  options: {
+    fontFamily: FMfont,
+    drawPercentageSign: true,
+    drawBubbles: true,
+    drawShadow: false,
+    size: 300,
+    borderWidth: 15,
+    backgroundColor: "#e2e2e2",
+    foregroundColor: "#fafafa",
+    foregroundFluidLayer: {
+      fillStyle: "#ff9000",
+      angularSpeed: 100,
+      maxAmplitude: 12,
+      frequency: 30,
+      horizontalSpeed: -75
+    },
+    backgroundFluidLayer: {
+      fillStyle: "#ffc880",
+      angularSpeed: 100,
+      maxAmplitude: 15,
+      frequency: 30,
+      horizontalSpeed: 75
     }
-  });
-}
+  }
+});
+
+//initialization python fluid meter
+var pythonFM = new FluidMeter();
+pythonFM.init({
+  targetContainer: document.getElementById("python-fluid-meter"),
+  fillPercentage: 75,
+  options: {
+    fontFamily: FMfont,
+    drawPercentageSign: true,
+    drawBubbles: true,
+    drawShadow: false,
+    size: 300,
+    borderWidth: 15,
+    backgroundColor: "#e2e2e2",
+    foregroundColor: "#fafafa",
+    foregroundFluidLayer: {
+      fillStyle: "#3572a5",
+      angularSpeed: 100,
+      maxAmplitude: 12,
+      frequency: 30,
+      horizontalSpeed: -75
+    },
+    backgroundFluidLayer: {
+      fillStyle: "#70b5ef",
+      angularSpeed: 100,
+      maxAmplitude: 15,
+      frequency: 30,
+      horizontalSpeed: 75
+    }
+  }
+});
+
 //handler for scroll top button
 var scrollTop = function () {
   if (platformIsMobile) {
@@ -1350,7 +1352,7 @@ var scrollTop = function () {
           }
         );
       })
-      .click(function () {
+      .click(function (event) {
         event.preventDefault();
         $("html, body").animate(
           {
@@ -1377,7 +1379,7 @@ var scrollTop = function () {
             .addClass("far");
         }
       )
-      .click(function () {
+      .click(function (event) {
         event.preventDefault();
         $("html, body").animate(
           {
